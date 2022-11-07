a0b165d86202827040ccfb86ed55ae7dc1cb7d97
agoltzman <106151463+agoltzman@users.noreply.github.com>
2022-11-07 15:22:08 +0200

    first commit

---
+++ src/Fireblocks-signer.ts
@@ -0,0 +1,96 @@
+import { FireblocksSDK, RawMessageData, TransactionArguments, TransactionOperation, PeerType, TransactionStatus } from "fireblocks-sdk";
+
+import { SignerOptions } from "@polkadot/api/submittable/types";
+import type { Signer, SignerResult } from "@polkadot/api/types";
+import type { SignerPayloadRaw } from "@polkadot/types/types";
+import { ApiPromise, WsProvider } from "@polkadot/api";
+import { assert, hexToU8a, u8aToHex } from "@polkadot/util";
+import { blake2AsHex } from '@polkadot/util-crypto';
+
+class FireblocksSigner implements Signer {
+    constructor(public fireblocks: FireblocksSDK, private vaultAccountId: string, private txNote?: string) {
+    }
+
+    public async signRaw({ data, type }: SignerPayloadRaw): Promise<SignerResult> {
+        return new Promise(async (resolve) => {
+
+            data = (data.length > (256 + 1) * 2) ? blake2AsHex(data) : data;
+
+            console.log('Payload: ' + data);
+            console.log('Type: ' + type);
+            console.log('hexToU8a(data): ' + hexToU8a(data));
+
+            const rawMessageData: RawMessageData = {
+                messages: [{
+                    content: data.substring(2)
+                }]
+            }
+
+            const tx: TransactionArguments = {
+                operation: TransactionOperation.RAW,
+                source: {
+                    type: PeerType.VAULT_ACCOUNT,
+                    id: this.vaultAccountId
+                },
+                assetId: 'KSM',
+                extraParameters: { rawMessageData },
+                note: this.txNote || ""
+            }
+
+            const txId = (await this.fireblocks.createTransaction(tx)).id;
+            console.log('TXID: ' + txId);
+
+            while ((await this.fireblocks.getTransactionById(txId)).status != TransactionStatus.COMPLETED) {
+                console.log((await this.fireblocks.getTransactionById(txId)).status);
+                setTimeout(() => { }, 4000);
+            }
+
+            const signedTx = (await this.fireblocks.getTransactionById(txId)).signedMessages;
+            if (signedTx != undefined) {
+                const signature = '0x00' + signedTx[0].signature.fullSig;
+                console.log('Signature: ' + signature);
+
+                resolve({ id: 1, signature });
+            }
+        });
+    }
+}
+
+export async function sendTransaction(fireblocks: FireblocksSDK, account: string, blocks: number | undefined, endpoint: string, [txName, ...params]: string[], vaultAccountId, txNote): Promise<void> {
+    const api = await ApiPromise.create({ provider: new WsProvider(endpoint) });
+
+    const [section, method] = txName.split('.');
+    assert(api.tx[section] && api.tx[section][method], `Unable to find method ${section}.${method}`);
+
+    const options: Partial<SignerOptions> = { signer: new FireblocksSigner(fireblocks, vaultAccountId, txNote) };
+
+    if (blocks === 0) {
+        // immortal extrinsic
+        options.era = 0;
+    } else if (blocks != null) {
+        // Get current block if we want to modify the number of blocks we have to sign
+        const signedBlock = await api.rpc.chain.getBlock();
+
+        options.blockHash = signedBlock.block.header.hash;
+        options.era = api.createType('ExtrinsicEra', {
+            current: signedBlock.block.header.number,
+            period: blocks
+        });
+    }
+
+    await api.tx[section][method](...params).signAndSend(account, options, (result): void => {
+
+        if (result.isInBlock || result.isFinalized) {
+
+            if (result.dispatchError?.isModule) {
+                const decoded = api.registry.findMetaError(result.dispatchError.asModule);
+                const { docs, name, section } = decoded;
+
+                console.log(`${section}.${name}: ${docs.join(' ')}`);   
+            }
+
+            console.log(JSON.stringify(result.toHuman(), null, 2));
+            process.exit(0);
+        }
+    });
+}
---
+++ src/ksm-staker.ts
@@ -0,0 +1,74 @@
+import { sendTransaction } from "./Fireblocks-signer";
+
+
+export  class KSMStaker {
+    constructor(public apiClient, public testnet = false) {
+    }
+
+    getAssetId () {
+        return this.testnet ? 'WND' : 'KSM';
+    }
+
+    getEndpoint() {
+        return this.testnet ? 'wss://westend-rpc.polkadot.io/' : 'wss://kusama-rpc.polkadot.io';
+    }
+
+    async getPermanentAddress(vaultAccountId) {
+        var depositAddress = await this.apiClient.getDepositAddresses(vaultAccountId, this.getAssetId());
+        return depositAddress[0].address;
+    }
+    
+
+    async sendTransaction({ vaultAccountId, params, txNote}: {vaultAccountId: string, params: string[], txNote?: string}) {
+        const permanentAddress = await this.getPermanentAddress(vaultAccountId);
+        return sendTransaction(this.apiClient, permanentAddress, 0, this.getEndpoint(), params , vaultAccountId, txNote);
+    }
+
+    async bond(vaultAccountId, amount?: number, controllerAddress?: string, rewardDestination?: string) {
+        if(!amount) {
+            const availableBalance = (await this.apiClient.getVaultAccountAsset(vaultAccountId, this.getAssetId())).available;
+            amount = Number.parseFloat(availableBalance);
+        } 
+
+        const txNote = controllerAddress ? `Bond ${amount} KSM to ${controllerAddress}` : `Bond ${amount} KSM`;
+        console.log(txNote);
+
+        await this.sendTransaction({
+            vaultAccountId,
+            params: [
+                'staking.bond', controllerAddress || await this.getPermanentAddress(vaultAccountId),
+                (amount * 1000000000000).toString(),
+                rewardDestination? rewardDestination: 'Stash'
+            ],
+            txNote
+        });
+    }
+    
+    async bondExtra(vaultAccountId, amount?: number) {
+        if(!amount) {
+            const availableBalance = await this.apiClient.getVaultAccountAsset(vaultAccountId, this.getAssetId()).available;
+            amount = Number.parseFloat(availableBalance);
+        }
+        await this.sendTransaction({ params: ['staking.bondExtra', (amount * 1000000000000).toString()], vaultAccountId, txNote: `Bond extra ${amount} KSM`});
+    }
+    
+    async unbond(vaultAccountId, amount?: number) {
+        await this.sendTransaction({params: ['staking.unbond',(amount * 1000000000000).toString()], vaultAccountId, txNote: `Unbonding ${amount} KSM`});
+    }
+    
+    async addProxy(vaultAccountId, proxyAddress) {
+        await this.sendTransaction({params: ['proxy.addProxy', proxyAddress, 'Staking', '0'], vaultAccountId, txNote: `Adding the following proxy: ${proxyAddress}`});
+    }
+
+    async chill(vaultAccountId) {
+        await this.sendTransaction({params: ['staking.chill'], vaultAccountId, txNote: `Chilling the controller account`});
+    }
+
+    async removeProxy(vaultAccountId, proxyAddress, proxyType?) {
+        await this.sendTransaction({params: ['proxy.removeProxy', proxyAddress, proxyType? proxyType: 'Staking', '0'], vaultAccountId, txNote: `Removing the following proxy: ${proxyAddress}`});
+    }
+    async withdrawUnbonded(vaultAccountId) {
+        await this.sendTransaction({params: ['staking.withdrawUnbonded', null], vaultAccountId, txNote: `Withdrawing Unbonded Funds`});
+    }
+}
+
